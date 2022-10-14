182cf5bbceb7496d8fd938851dcfceef509c94bf
Noah King <109630641+NoahBroah@users.noreply.github.com>
2022-10-14 11:37:36 -0400

    Import BrowserRoute / and make starter components

--- frontend/phase-3-frontend/src/App.js
+++ frontend/phase-3-frontend/src/App.js
@@ -1,12 +1,21 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./NavBar";
-
+import Home from "./Home";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-      <header className="App-header">Hello</header>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact patch="/login">
+          <Login/>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- frontend/phase-3-frontend/src/NavBar.js
+++ frontend/phase-3-frontend/src/NavBar.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <div>
+        <NavLink to="/" exact>
+            Home
+        </NavLink>
+        <NavLink to="/login" exact>
+            Login
+        </NavLink>
+    </div>
   )
 }
 
